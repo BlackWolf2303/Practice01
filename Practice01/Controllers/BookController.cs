aac83b8d0060264161d0452d0193f0c2106a6905
BWolf <haiht.aptech@gmail.com>
2018-08-12 21:06:59 +0700

    Commit lan 2

---
+++ Practice01/Controllers/BookController.cs
@@ -0,0 +1,38 @@
+﻿using Practice01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice01.Controllers
+{
+    public class BookController : Controller
+    {
+        List<Book> books = new List<Book>()
+        {
+            new Book()
+            {   
+                BookId  = "P12546",
+                Title   = "Kungfu Panda",
+                Author  = "Thanh Hai",
+                Price   = 50000,
+                Picture = "Content/image/books/01.jpg"
+            },
+            new Book()
+            {
+                BookId  = "P47573",
+                Title   = "Silicon Valey",
+                Author  = "American",
+                Price   = 451000,
+                Picture = "Content/image/books/02.jpeg"
+            },
+        };
+        // GET: Book
+        public ActionResult Index()
+        {
+            ViewBag.books = books;
+            return View();
+        }
+    }
+}
